5cfed1e670959d8b2a04706b2e4d287629943aff
kauthar <kowsardalmar@gmail.com>
2022-08-25 12:12:18 +0300

    calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+ var onclick="forms.screen.value +='1',vib()"
+ var onclick="forms.screen.value+='2',vib()"
+ var onclick="forms.screen.value+='3',vib()"
+ var onclick="forms.screen.value ='',vib()"
+ var onclick="forms.screen.value+='4',vib()"
+ var onclick="forms.screen.value+='5',vib()"
+ var onclick="forms.screen.value+='6',vib()"
+
+ var onclick="forms.screen.value+='+',vib()"
+ var onclick="forms.screen.value+='7',vib()"
+ var onclick="forms.screen.value+='8',vib()"
+ var onclick="forms.screen.value+='9',vib()"
+
+ var onclick="forms.screen.value+='-',vib()"
+ var onclick="forms.screen.value+='.',vib()"
+ var onclick="forms.screen.value+='0',vib()"
+
+ var onclick="forms.screen.value = eval(forms.screen.value),vib()"
+ var onclick="forms.screen.value+='*',vib()"
+
+ var onclick="forms.screen.value+='(',vib()"
+
+ var onclick="forms.screen.value+=')',vib()"
+
+var  onclick="forms.screen.value+='%',vib()"
+
+var  onclick="forms.screen.value+='/',vib()"
+
+
